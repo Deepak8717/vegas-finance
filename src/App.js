addfaf82b52aa5aa63b6d7d13384725f09228204
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2022-07-16 16:01:38 +0300

    add web3 to test bundle with webpack5

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Web3 from 'web3';
 
 function App() {
+
+  //eslint-disable-next-line
+  const web3Instance = new Web3('http://localhost:8545');
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Web3 is imported correctly and is working with Webpack 5
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
